a89a8a505b88dc113c9b47c9c1dec356f947b8ff
Julien Vernet <julien@vernet.me>
2018-11-18 15:25:51 +0700

    add smooth scroll

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import './scss/main.scss'
 import $ from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+import SmoothScroll from "smooth-scroll";
+
+const scroll = new SmoothScroll("[data-scroll]");
