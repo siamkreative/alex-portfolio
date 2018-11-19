2f20140b44eb1e2f5790d2c3886872e2b1ae9f9d
Julien Vernet <julien@vernet.me>
2018-11-19 09:15:21 +0700

    remove unused dependencies

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import './scss/main.scss'
-import $ from "jquery";
-import popper from "popper.js";
-import bootstrap from "bootstrap";
-import SmoothScroll from "smooth-scroll";
+import bootstrap from "bootstrap"
+import SmoothScroll from "smooth-scroll"
 
-const scroll = new SmoothScroll("[data-scroll]");
+const scroll = new SmoothScroll("[data-scroll]")
