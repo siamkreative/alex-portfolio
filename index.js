e6ad2e8eb7537bef7f9b982009023234225d40c6
Julien Vernet <julien@vernet.me>
2018-11-20 18:27:44 +0700

    specify fixed header selector. closes #5

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import './scss/main.scss'
 import SmoothScroll from 'smooth-scroll'
 
 // Smooth scroll
-const scroll = new SmoothScroll('[data-scroll]')
+const scroll = new SmoothScroll('[data-scroll]', {
+  header: '#mainnav'
+})
 
 // Copyright info
 console.log('Built with love by YOSA https://yosadigital.com')
