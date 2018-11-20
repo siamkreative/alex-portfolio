aa4fd0712573ee21ae93b5d617e7e0d54b37d4ac
Julien Vernet <julien@vernet.me>
2018-11-20 11:21:12 +0700

    add © info

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import './scss/main.scss'
-import bootstrap from "bootstrap"
-import SmoothScroll from "smooth-scroll"
+import bootstrap from 'bootstrap'
+import SmoothScroll from 'smooth-scroll'
 
-const scroll = new SmoothScroll("[data-scroll]")
+// Smooth scroll
+const scroll = new SmoothScroll('[data-scroll]')
+
+// Copyright info
+console.log('Built with love by YOSA https://yosadigital.com')
