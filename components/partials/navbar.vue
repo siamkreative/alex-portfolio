09b8a0ebbdbdc754e62cd19f5ce5d2b237e13955
Julien Vernet <julien@vernet.me>
2018-11-24 19:14:03 +0700

    Conditional navigation logic

--- components/partials/navbar.vue
+++ components/partials/navbar.vue
@@ -8,8 +8,12 @@
           <a class="nav-link" data-scroll href="#about">About</a>
         </li>
       </ul>
-      <!-- Below does not work because does not get updated on route change -->
-      <router-link v-if="innerPage" to="/">Go to home</router-link>
+      <!-- Conditional navigation logic -->
+      <router-link v-if="this.$route.path !== '/'" to="/" class="logo nav-link mx-auto d-none d-sm-block">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="icon" width="100" height="60">
+          <use xlink:href="#logo"></use>
+        </svg>      
+      </router-link>
       <a v-else class="logo nav-link mx-auto d-none d-sm-block" data-scroll="scroll" href="#top">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="icon" width="100" height="60">
           <use xlink:href="#logo"></use>
@@ -25,25 +29,6 @@
 
 <script>
 export default {
-  name: 'navbar',
-  data () {
-    return {
-      innerPage: false
-    }
-  },
-  mounted (){
-    if (this.$route.path !== '/') {
-      console.log('This is not the index page')
-      this.innerPage == true 
-    }
-  },  
-  watch:{
-    $route (to, from){
-      if (to.path !== '/') {
-        console.log('This is not the index page')
-        this.innerPage == true 
-      }
-    }
-  }
+  name: 'navbar'
 }
 </script>
